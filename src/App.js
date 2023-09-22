de50459cde8e7cab1448fef340dbcc2b7b6a1d26
stahsin00 <lamisa1998@hotmail.com>
2023-09-22 15:09:42 -0700

    practicing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,87 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, FormControl, Button, FormGroup, Navbar, Nav, NavItem} from 'react-bootstrap';
+
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  async function sendMessage(m) {
+    const response = await fetch(
+          "https://api.openai.com/v1/chat/completions",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + "" //"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"
+            },
+            body: JSON.stringify({
+              "model" : "gpt-3.5-turbo",
+              "messages": [{
+                "role" : "user",
+                "content" : m
+              }]
+            })
+          }
+    )
+
+    console.log(response);
+
+    return response.json();
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const response = await sendMessage(inputValue);
+      const apiMessage = response.choices[0].message.content;
+      setMessage(`${message}\n${inputValue}\n${apiMessage}`);
+    } catch (error) {
+      setMessage(`${message}\n${inputValue}\n${error}`);
+    }
+
+    setInputValue("");
+  }
+
+  function handleInputChange(e) {
+    setInputValue(e.target.value);
+  }
+
   return (
-    <h2>Langara Hacks 2023</h2>
+    <div>
+      <header>
+      <h2>New chat</h2>
+      </header>
+      <main className='main-content'>
+      <Navbar>
+        <Nav className='flex-column'>
+          <NavItem>
+            1
+          </NavItem>
+          <NavItem>
+            2
+          </NavItem>
+          <NavItem>
+            3
+          </NavItem>
+        </Nav>
+      </Navbar>
+      <div className='content'>
+      <div style={{ whiteSpace: 'pre-line' }}>
+        {message}
+      </div>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Form.Control type="text" placeholder="Send a message" value={inputValue} onChange={handleInputChange} />
+        </FormGroup>
+        <Button type="submit">Send</Button>
+      </Form>
+      </div>
+    </main>
+    </div>
   );
 }
 
