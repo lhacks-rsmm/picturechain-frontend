04b796de5d855538f4a5ce7579a704a074e81945
stahsin00 <lamisa1998@hotmail.com>
2023-09-23 12:10:19 -0700

    moved login to appheader

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,14 +1,22 @@
 import { Grid, Button } from "@mui/material";
 import {Link} from "react-router-dom";
-import React from "react";
+import {React, useState} from "react";
+import AuthModal from "./AuthModal";
 
 function AppHeader() {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
-        <Grid container spacing={3}>
-            <Grid item xs={2}><Link to="/home">Picture Chain</Link></Grid>
-            <Grid item xs={8}/>
-            <Grid item xs={2}><Button variant="outlined">Login</Button></Grid>
-        </Grid>
+        <>
+            <AuthModal open={open} handleClose={handleClose}/>
+            <Grid container spacing={3}>
+                <Grid item xs={2}><Link to="/home">Picture Chain</Link></Grid>
+                <Grid item xs={8}/>
+                <Grid item xs={2}><Button variant="outlined" onClick={handleOpen}>Login</Button></Grid>
+            </Grid>
+        </>
     )
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,15 @@
 import Grid from "@mui/system/Unstable_Grid/Grid";
 import Button from "@mui/material/Button";
 import { useState } from "react";
-import AuthModal from "../components/AuthModal";
+import AppHeader from "../components/AppHeader";
+import {Link} from "react-router-dom"
 
 
 export default function Home() {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-
     return (
         <>
-        <AuthModal open={open} handleClose={handleClose}/>
         <Grid container spacing={3}>
-            <Grid item xs={2}>Picture Chain</Grid>
-            <Grid item xs={8}/>
-            <Grid item xs={2}><Button variant="outlined" onClick={handleOpen}>Login</Button></Grid>
+        <Grid item xs={12}><AppHeader /></Grid>
             <Grid item xs={1} />
             <Grid item xs={10} className="img-container">
                 <div style={{height: "512px", width:"512px"}}>
