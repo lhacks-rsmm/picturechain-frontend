e293444c3ad83499966363b26767d78d7d73e0e6
miguel-fg <miguelfierro55@gmail.com>
2023-09-23 12:56:59 -0700

    initial colour placement

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,5 @@
 import Grid from "@mui/system/Unstable_Grid/Grid";
 import Button from "@mui/material/Button";
-import { useState } from "react";
 import AppHeader from "../components/AppHeader";
 import {Link} from "react-router-dom"
 
@@ -12,15 +11,15 @@ export default function Home() {
         <Grid item xs={12}><AppHeader /></Grid>
             <Grid item xs={1} />
             <Grid item xs={10} className="img-container">
-                <div style={{height: "512px", width:"512px"}}>
+                <div style={{height: "512px", width:"512px"}} className="featured-drawing">
 
                 Featured drawing
                 </div>
                 </Grid>
             <Grid item xs={1} />
             <Grid item xs={3} />
-            <Grid item xs={3}><Link to="/dashboard"><Button variant="contained">Public</Button></Link></Grid>
-            <Grid item xs={3}><Button variant="contained">Private</Button></Grid>
+            <Grid item xs={3}><Link to="/dashboard"><Button variant="contained" className="lobby-button">Public</Button></Link></Grid>
+            <Grid item xs={3}><Button variant="contained" className="lobby-button">Private</Button></Grid>
             <Grid item xs={3} />
         </Grid>
         </>
